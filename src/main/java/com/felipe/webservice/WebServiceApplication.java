5d020b02f10d15d417c0314782fcdd8efdcb8bbd
Felipefams <augustofams@gmail.com>
2023-02-23 17:37:19 -0300

    User && userResource classes created

--- src/main/java/com/felipe/webservice/WebServiceApplication.java
+++ src/main/java/com/felipe/webservice/WebServiceApplication.java
@@ -8,7 +8,6 @@ public class WebServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebServiceApplication.class, args);
-        System.out.println("teste");
     }
 
 }
---
+++ src/main/java/com/felipe/webservice/entities/User.java
@@ -0,0 +1,21 @@
+package com.felipe.webservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User(){}
+
+}
