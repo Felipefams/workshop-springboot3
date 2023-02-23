15ed1eeac6bac4720478195fddbbb28bdd3b6da1
Felipefams <augustofams@gmail.com>
2023-02-23 19:13:26 -0300

    dependency injection and database seeding

---
+++ src/main/java/com/felipe/webservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.felipe.webservice.repositories;
+
+import com.felipe.webservice.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//we do not need to implement this interface, because Jpa already does this for us
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
