24f9398d24ade4c36ed771dcc4da8d0f389a0ad1
Felipefams <augustofams@gmail.com>
2023-02-23 16:03:00 -0300

    first commit

---
+++ src/main/java/com/felipe/webservice/WebServiceApplication.java
@@ -0,0 +1,14 @@
+package com.felipe.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceApplication.class, args);
+        System.out.println("teste");
+    }
+
+}
