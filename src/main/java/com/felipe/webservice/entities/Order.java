a753741666a9826b9c06744ad39f2574d33f6ee9
Felipefams <augustofams@gmail.com>
2023-02-27 17:57:30 -0300

    Enum OrderStatus

--- src/main/java/com/felipe/webservice/entities/Order.java
+++ src/main/java/com/felipe/webservice/entities/Order.java
@@ -2,6 +2,7 @@ package com.felipe.webservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.felipe.webservice.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    @Enumerated(EnumType.ORDINAL)
+    private OrderStatus orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
---
+++ src/main/java/com/felipe/webservice/entities/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.felipe.webservice.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    PACKAGING,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
